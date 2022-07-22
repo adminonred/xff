ab7af89a30a9b063f064720fd97545a91af91a51
stu <stu@123.com>
2022-07-22 14:50:51 +0800

    4

--- aaa/src/main/java/org/example/App.java
+++ aaa/src/main/java/org/example/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.out.println("hh");
+        System.out.println(1);
     }
 }
